73fb80133cb6ba03215b26a9072d37172ba1b867
Cristian Rodriguez <alejo.lili03@gmail.com>
2025-10-04 17:14:33 -0500

    Desarrollo ejercicio 6

---
+++ reforzamiento-typescript/src/ejercicios/ejercicio6/06-obj-destructuring.ts
@@ -0,0 +1,14 @@
+// Reutilizamos un objeto estudiante
+const estudiante = {
+    nombre: "Juan Pérez",
+    edad: 21,
+    carrera: "Ingeniería en Sistemas",
+    semestre: 6
+};
+
+// Desestructuración de propiedades del objeto
+const { nombre, carrera } = estudiante;
+
+// Mostrar en consola
+console.log("Nombre del estudiante:", nombre);
+console.log("Carrera:", carrera);
