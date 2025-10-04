d319c9d946d2706d7e392f5d4a12a03b9b121e87
Aria-707 <arirg100@gmail.com>
2025-10-04 16:46:09 -0500

    Cambio en la impresión por consola

--- reforzamiento-typescript/src/ejercicios/ejercicio2/02-template-string.ts
+++ reforzamiento-typescript/src/ejercicios/ejercicio2/02-template-string.ts
@@ -3,8 +3,6 @@ const course = 'Ingeniería de Sistemas'
 const semester = "IX"
 
 //Concatenar mensaje
-const message=`
-El estudiante es ${nameStudent} se encuentra en el semestre ${semester} de la carrera de ${course}.
-`
+const message=`El estudiante es ${nameStudent} se encuentra en el semestre ${semester} de la carrera de ${course}.`
 //Impresión del mensaje
 console. log(message) ;
