9791c2f2a42f206705fe94e3665fc4c77402cc4c
Aria-707 <arirg100@gmail.com>
2025-10-04 17:29:31 -0500

    Desarrollo del ejercicio 4

--- reforzamiento-typescript/src/main.ts
+++ reforzamiento-typescript/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import './ejercicios/ejercicio1/01-variables'
 import './ejercicios/ejercicio2/02-template-string'
 import './ejercicios/ejercicio3/03-object-literal'
+import './ejercicios/ejercicio4/04-arrays'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
